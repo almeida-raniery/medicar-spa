7650d75ba319a3f7cf19a994919da12ddff207c3
raniery <ranie-ry@outlook.com>
2022-11-25 10:27:36 -0300

    fix: Logo agora redireciona para página inicial

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,17 @@
 import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
+import {LinkContainer} from "react-router-bootstrap"
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import SearchForm from "../Forms/Search Form";
+import { Container } from "react-bootstrap";
 
 function Header() {
   return (
     <Navbar>
-      <Container className="container">
-        <NavbarBrand href="#" className="navbar-brand">MedSearch</NavbarBrand>
-        <SearchForm />
+      <Container>
+      <LinkContainer to="/">
+        <NavbarBrand>MedSearch</NavbarBrand>
+      </LinkContainer>
+      <SearchForm />
       </Container>
     </Navbar>
   );
