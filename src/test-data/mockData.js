e7be31fcc5c180a1587854382459461caa7b9876
raniery <ranie-ry@outlook.com>
2022-11-28 02:52:08 -0300

    feat: testes da página de resultados de pesquisa

--- src/test-data/mockData.js
+++ src/test-data/mockData.js
@@ -1,12 +1,23 @@
 export const medicationData = {
-    nomeComercial: "HISTAMIN",
-    classesTerapeuticas: ["ANTI-HISTAMINICOS SISTEMICOS"],
-    principioAtivo: "MALEATO DE DEXCLORFENIRAMINA",
-    medicamentoReferencia: "Histamin",
-    categoriaRegulatoria: "similar",
-    empresa: {
-      razaoSocial: "BRAINFARMA INDÚSTRIA QUÍMICA E FARMACÊUTICA S.A",
-    },
-    codigoBulaPaciente:
-      "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxNDk4NDIwMSIsIm5iZiI6MTY2OTYwMTU3NiwiZXhwIjoxNjY5NjAxODc2fQ.dbTjOvEbSPwKqpQuiAjh-2xWxFOVbmnHTqVdAdneVH8SArruUYua5ciofoCKZwikq0JL6v_pJO8CF3SLSG7SCA",
-}
+  nomeComercial: "HISTAMIN",
+  classesTerapeuticas: ["ANTI-HISTAMINICOS SISTEMICOS"],
+  principioAtivo: "MALEATO DE DEXCLORFENIRAMINA",
+  medicamentoReferencia: "Histamin",
+  categoriaRegulatoria: "similar",
+  empresa: {
+    razaoSocial: "BRAINFARMA INDÚSTRIA QUÍMICA E FARMACÊUTICA S.A",
+  },
+  codigoBulaPaciente:
+    "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiIxNDk4NDIwMSIsIm5iZiI6MTY2OTYwMTU3NiwiZXhwIjoxNjY5NjAxODc2fQ.dbTjOvEbSPwKqpQuiAjh-2xWxFOVbmnHTqVdAdneVH8SArruUYua5ciofoCKZwikq0JL6v_pJO8CF3SLSG7SCA",
+};
+
+export const searchData = [
+  {
+    nomeProduto: "DIPIRONA",
+    razaoSocial: "BAYER S.A",
+  },
+  {
+    nomeProduto: "dipirona",
+    razaoSocial: "BRAINFARMA INDÚSTRIA QUÍMICA E FARMACÊUTICA S.A",
+  },
+];
